7cd1d29b871469708ccf5435175716e3c7c63951
sijalneupane <sijalneupane5@gmail.com>
2025-10-28 22:31:00 +0545

    feat: initialize FastAPI application and include health check router

--- app/main.py
+++ app/main.py
@@ -1,2 +1,8 @@
-def hi():
-    print("Hello, World!")
+from fastapi import FastAPI
+
+from app.api.v1.routers import app_health
+from app.core.config import settings
+
+app = FastAPI()
+
+app.include_router(app_health.router, prefix=settings.API_V1_STR)
