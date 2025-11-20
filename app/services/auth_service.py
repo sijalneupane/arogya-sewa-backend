a0862a9f4fc9d514fee1e4d6b0aa6d1ac5201f6f
sijalneupane <sijalneupane5@gmail.com>
2025-11-20 22:01:28 +0545

    fix: improve error handling in user authentication process

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -12,18 +12,20 @@ from app.services.user_service import get_user_by_email
 async def authenticate_user(db: AsyncSession, email: str, password: str):
     user = await get_user_by_email(db, email)
     if not user:
-        return None
-    if not verify_password(password, user.hashed_password):
-        return None
+        raise HTTPException(status_code=401, detail="Invalid email ")
+    if not verify_password(password, user.password):
+        raise HTTPException(status_code=401, detail="Invalid password")
     return user
 
 
 async def login_user(db: AsyncSession, email: str, password: str):
-    user = await authenticate_user(db, email, password)
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid email or password")
+    try:
+        user = await authenticate_user(db, email, password)
+        if not user:
+            raise HTTPException(status_code=401, detail="Invalid email or password")
 
-    access = create_access_token({"sub": user.id, "role": user.role})
-    refresh = create_refresh_token({"sub": user.id})
-
+        access = create_access_token({"sub": user.id, "role": user.role.role})
+        refresh = create_refresh_token({"sub": user.id})
+    except HTTPException as e:
+        raise e
     return access, refresh, user
