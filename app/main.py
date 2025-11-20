f3f4971804242984bbc541dfb2be2a30daa4277b
sijalneupane <sijalneupane5@gmail.com>
2025-11-20 22:07:00 +0545

    feat: add user route for user management

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-from api.v1.routers import auth_route
-from app.api.v1.routers import app_health, user
+from api.v1.routers import auth_route, user_route
+from app.api.v1.routers import app_health
 from app.core.config import settings
 
 app = FastAPI(title=settings.APP_NAME, version=settings.VERSION)
 
 app.include_router(app_health.router, prefix=settings.API_V1_STR)
 app.include_router(auth_route.router, prefix=settings.API_V1_STR)
-app.include_router(user.router, prefix=settings.API_V1_STR)
+app.include_router(user_route.router, prefix=settings.API_V1_STR)
