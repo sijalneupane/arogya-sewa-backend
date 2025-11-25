11fceb5dc11f28b6281d238d32c9267dddde9249
sijalneupane <sijalneupane5@gmail.com>
2025-11-25 06:10:18 +0545

    fix: correct import statements for auth_router and user_router in main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-from app.api.v1.routers import app_health, auth_route, user_route
+from app.api.v1.routers import app_health, auth_router, user_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.APP_NAME, version=settings.VERSION)
 
 app.include_router(app_health.router, prefix=settings.API_V1_STR)
-app.include_router(auth_route.router, prefix=settings.API_V1_STR)
-app.include_router(user_route.router, prefix=settings.API_V1_STR)
+app.include_router(auth_router.router, prefix=settings.API_V1_STR)
+app.include_router(user_router.router, prefix=settings.API_V1_STR)
