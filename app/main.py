561005c28acc953fc6d608860c3e892689c22794
sijalneupane <sijalneupane5@gmail.com>
2025-12-05 15:17:33 +0000

    feat: add global exception handling and validation error responses in main application

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from os import error
+from fastapi import FastAPI, HTTPException, status
+from fastapi.exceptions import RequestValidationError, ResponseValidationError
+from fastapi.responses import JSONResponse
 
 from app.core.app import app_health
 from app.core.config import settings
@@ -7,6 +10,45 @@ from app.modules.user.v1 import router as user_router
 from app.modules.hospital.v1 import router as hospital_router
 
 app = FastAPI(title=settings.APP_NAME, version=settings.VERSION)
+
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": str(exc)},
+    )
+
+
+@app.exception_handler(
+    RequestValidationError,
+)
+async def validation_exception_handler(request, exc: RequestValidationError):
+    error_list = []
+    for err in exc.errors():
+        error_list.append(err["msg"])
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={
+            "detail": error_list,
+            "status": status.HTTP_400_BAD_REQUEST,
+        },
+    )
+
+
+@app.exception_handler(ResponseValidationError)
+async def response_validation_exception_handler(request, exc: ResponseValidationError):
+    error_list = []
+    for err in exc.errors():
+        error_list.append(err["msg"])
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={
+            "detail": error_list,
+            "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+        },
+    )
+
 
 app.include_router(app_health.router, prefix=settings.API_V1_STR)
 app.include_router(auth_router.router, prefix=settings.API_V1_STR)
